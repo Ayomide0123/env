d468bd64172dcd8e1c63cfe8af1b90cb4b57062b
Ayomide <64165980+Ayomide0123@users.noreply.github.com>
2022-06-15 01:10:14 +0100

    Update models.py

--- I4G023533GXR/blog/models.py
+++ I4G023533GXR/blog/models.py
@@ -11,8 +11,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     author = models.ForeignKey(
-        get_user_model(),
-        on_delete=models.CASCADE
+        get_user_model()
     )
     created_date = models.DateTimeField()
     published_date = models.DateTimeField()
