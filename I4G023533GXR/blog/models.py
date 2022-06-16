229d2316e447a25ed426b2f7064ff14f517258b1
Ayomide <64165980+Ayomide0123@users.noreply.github.com>
2022-06-16 08:43:47 +0100

    Update models.py

--- I4G023533GXR/blog/models.py
+++ I4G023533GXR/blog/models.py
@@ -2,15 +2,15 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 # Create your models here.
+...
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     author = models.ForeignKey(
-        get_user_model()
+        get_user_model(),
         on_delete=models.CASCADE
     )
     created_date = models.DateTimeField()
     published_date = models.DateTimeField()
-
-
