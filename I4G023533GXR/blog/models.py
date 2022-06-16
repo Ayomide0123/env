8d7ef30a7129fa21c383bc20ee79f7cdf4da5bc9
Ayomide <64165980+Ayomide0123@users.noreply.github.com>
2022-06-16 08:41:53 +0100

    Update models.py

--- I4G023533GXR/blog/models.py
+++ I4G023533GXR/blog/models.py
@@ -1,11 +1,7 @@
 from django.db import models
-from django.template.defaultfilters import slugify
 from django.contrib.auth import get_user_model
-from django.urls import reverse
 
-
-...
-
+# Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -17,4 +13,4 @@ class Post(models.Model):
     created_date = models.DateTimeField()
     published_date = models.DateTimeField()
 
-# Create your models here.
+
